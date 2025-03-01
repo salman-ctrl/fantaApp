4e840bf9bbc05f0b371b948b644e31d0fc1e7a82
salman-ctrl <zirmanvictory@gmail.com>
2025-03-01 14:45:12 +0700

    tambah section footer

---
+++ src/component/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='h-32 w-full bg-primary'>
+            <h1>Copy Write</h1>
+        </div>
+    )
+}
+
+export default Footer
