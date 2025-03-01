0d3fdd422b19fdc115d610f71a5875423c909891
salman-ctrl <zirmanvictory@gmail.com>
2025-03-01 14:50:10 +0700

    edit halaman hero

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from './Navbar';
 import aleMangga from '../assets/aleMangga.png'
+import aleJeruk from '../assets/aleJeruk.png'
+
 
 const Hero = () => {
     return (
@@ -18,6 +20,11 @@ const Hero = () => {
             <div className='absolute bottom-0 right-40'>
                 <img
                     src={aleMangga}
+                    alt=""
+                    className="w-90 h-120 hover:scale-105 motion-safe:animate-bounce hover:[animation-play-state:paused] transition-transform duration-700 ease-in-out"
+                />
+                <img
+                    src={aleJeruk}
                     alt=""
                     className="w-90 h-120 hover:scale-105 motion-safe:animate-bounce hover:[animation-play-state:paused] transition-transform duration-700 ease-in-out"
                 />
