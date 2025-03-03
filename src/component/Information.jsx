6afe50cddf6e1f8908294a7a004362ad02d7bb47
salman-ctrl <zirmanvictory@gmail.com>
2025-03-03 23:25:18 +0700

    memulai edit product section

--- src/component/Information.jsx
+++ src/component/Information.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import aleMangga from "../assets/"
 
 const Information = () => {
     return (
         <div>
             <h1>Information</h1>
+            <div className='container'>
+                <img src="" alt="" />
+            </div>
         </div>
     )
 }
--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import aleJeruk from '../assets/aleJeruk.png'
+import aleAnggur from '../assets/aleAnggur.png'
+import aleStroberi from '../assets/aleStroberi.png'
 
 const Product = () => {
     return (
