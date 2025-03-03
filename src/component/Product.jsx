7c2b17e3d2edf9c63ee1594d6335d6ee56d6462b
salman-ctrl <zirmanvictory@gmail.com>
2025-03-03 23:39:30 +0700

    edit style card

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -10,7 +10,8 @@ const dataProduct = [
 const Product = () => {
     return (
         <div className='h-screen w-screen '>
-            <div className='h-full w-full flex justify-center items-center space-x-2.5'>
+            <h2 className='text-3xl font-merienda text-center pt-30'>Produk Ale</h2>
+            <div className='h-full w-full flex justify-center pt-10 space-x-2.5 text-center'>
                 <div className='container w-40 h-50 bg-primary'>
                     <img src={aleJeruk} alt="" className='w-full h-30' />
                     <p className='text-white '>Jeruk</p>
