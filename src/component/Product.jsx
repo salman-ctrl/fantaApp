8261f2b908ec87c585dce65e9b439af478094a5e
salman-ctrl <zirmanvictory@gmail.com>
2025-03-03 23:36:06 +0700

    edit style card

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -3,9 +3,13 @@ import aleJeruk from '../assets/aleJeruk.png'
 import aleAnggur from '../assets/aleAnggur.png'
 import aleStroberi from '../assets/aleStroberi.png'
 
+const dataProduct = [
+
+]
+
 const Product = () => {
     return (
-        <div className='h-screen w-screen bg-red-500'>
+        <div className='h-screen w-screen '>
             <div className='h-full w-full flex justify-center items-center space-x-2.5'>
                 <div className='container w-40 h-50 bg-primary'>
                     <img src={aleJeruk} alt="" className='w-full h-30' />
