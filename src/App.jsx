78d3315b59f71a73c8b36f7b0a7cfa0af356143b
salman-ctrl <zirmanvictory@gmail.com>
2025-03-03 12:53:37 +0700

    edit my app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Hero from './component/Hero'
 import Information from './component/Information'
+import Product from "./component/Product"
 
 function App() {
   return (
     <>
-
       <Hero />
+      <Product />
       <Information />
     </>
-
   )
 }
 export default App
