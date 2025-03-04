2d00937f9716248f36dd1a74029c6e41b2142f5a
salman-ctrl <zirmanvictory@gmail.com>
2025-03-04 21:55:35 +0700

    change style halaman product

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -12,15 +12,15 @@ const Product = () => {
         <div className='h-screen w-screen '>
             <h2 className='text-3xl font-merienda text-center pt-30'>Produk Ale</h2>
             <div className='h-full w-full flex justify-center pt-10 space-x-2.5 text-center'>
-                <div className='container w-40 h-50 shadow-lg'>
+                <div className='container w-40 h-50 shadow-lg bg-white'>
                     <img src={aleJeruk} alt="" className='w-full h-30' />
                     <p className='text-white font-merienda '>Jeruk</p>
                 </div>
-                <div className='container w-40 h-50 shadow-lg'>
+                <div className='container w-40 h-50 shadow-lg bg-white'>
                     <img src={aleAnggur} alt="" className='w-full h-30' />
                     <p className='text-white font-merienda'>Mangga</p>
                 </div>
-                <div className='container w-40 h-50 shadow-lg'>
+                <div className='container w-40 h-50 shadow-lg bg-white'>
                     <img src={aleStroberi} alt="" className='w-full h-30' />
                     <p className='text-white font-merienda'>Mangga</p>
                 </div>
