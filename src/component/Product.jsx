96b47ee4ec2e5f0230f8a002403337779a33c9ac
salman-ctrl <zirmanvictory@gmail.com>
2025-03-04 21:52:55 +0700

    change image

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -12,11 +12,11 @@ const Product = () => {
         <div className='h-screen w-screen '>
             <h2 className='text-3xl font-merienda text-center pt-30'>Produk Ale</h2>
             <div className='h-full w-full flex justify-center pt-10 space-x-2.5 text-center'>
-                <div className='container w-40 h-50 bg-primary'>
+                <div className='container w-40 h-50 shadow-lg'>
                     <img src={aleJeruk} alt="" className='w-full h-30' />
                     <p className='text-white '>Jeruk</p>
                 </div>
-                <div className='container w-40 h-50 bg-primary'>
+                <div className='container w-40 h-50 shadow-lg'>
                     <img src={aleAnggur} alt="" className='w-full h-30' />
                     <p className='text-white '>Mangga</p>
                 </div>
