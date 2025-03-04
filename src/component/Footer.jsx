1f3daa413ae2d6c3731f72c58bbe9060c378b236
salman-ctrl <zirmanvictory@gmail.com>
2025-03-04 22:08:37 +0700

    footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='h-32 w-full bg-primary'>
-            <h1>Copy Write</h1>
+            <h1>Copy Write 2025</h1>
         </div>
     )
 }
