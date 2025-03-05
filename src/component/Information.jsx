027c1a1cbec34823e4e567780155a267691f6625
salman-ctrl <zirmanvictory@gmail.com>
2025-03-05 22:32:43 +0700

    edit information section

--- src/component/Information.jsx
+++ src/component/Information.jsx
@@ -5,9 +5,6 @@ const Information = () => {
         <div>
             <h1>Information</h1>
             <div className='container'>
-                <img src="" alt="" />
-                <h1>John Doe</h1>
-                <p>Deskripsi</p>
 
             </div>
         </div>
