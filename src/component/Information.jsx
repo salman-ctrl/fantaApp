6c5ddca8cabd8cbccdb3fe250152a5206e294400
salman-ctrl <zirmanvictory@gmail.com>
2025-03-05 22:28:52 +0700

    add product

--- src/component/Information.jsx
+++ src/component/Information.jsx
@@ -6,6 +6,8 @@ const Information = () => {
             <h1>Information</h1>
             <div className='container'>
                 <img src="" alt="" />
+                <p>Information about the project</p>
+                <p>Information about the project</p>
             </div>
         </div>
     )
