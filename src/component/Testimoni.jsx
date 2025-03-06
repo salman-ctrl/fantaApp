742bf08fd704ae77e0f9f5023df761fef78adf03
salman-ctrl <zirmanvictory@gmail.com>
2025-03-06 23:16:44 +0700

    testimoni section

--- src/component/Testimoni.jsx
+++ src/component/Testimoni.jsx
@@ -2,7 +2,7 @@ import { data } from 'framer-motion/client'
 import React from 'react'
 
 const dataClient = [
-    { id: 1, name: 'John', age: 25 },
+    { id: 1, name: 'John', age: 25, deskripsi: "loremakslak akjs" },
     { id: 2, name: 'Jane', age: 30 },
     { id: 3, name: 'Bob', age: 35 },
 
@@ -18,8 +18,8 @@ const Testimoni = () => {
                 <div>
                     {dataClient.map((item) => (
                         <div key={dataClient.id}>
-                            <h1 className='text-center font-merienda text-white'>{dataClient.name}</h1>
-                            <p>{dataClient.age}</p>
+                            <h1 className='text-center text-lg font-merienda text-white'>{dataClient.name}</h1>
+                            <p className='text-sm'>{dataClient.age}</p>
                         </div>
                     ))}
                 </div>
