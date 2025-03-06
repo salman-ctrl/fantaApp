e19938a8ce01ac6406ba44711a9b5cda71d1d772
salman-ctrl <zirmanvictory@gmail.com>
2025-03-06 23:11:28 +0700

    testimoni section

--- src/component/Testimoni.jsx
+++ src/component/Testimoni.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
+const dataClient = [
+    { id: 1, name: 'John', age: 25 },
+    { id: 2, name: 'Jane', age: 30 },
+    { id: 3, name: 'Bob', age: 35 },
+
+
+]
+
 const Testimoni = () => {
     return (
         <div>
+            <h1>Apa kata mereka?</h1>
+
+            <div>
+
+            </div>
         </div>
     )
 }
