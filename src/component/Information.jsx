c33082d13893b147998207323a1a5eac13dc4457
salman-ctrl <zirmanvictory@gmail.com>
2025-03-06 23:01:59 +0700

    edit information section

--- src/component/Information.jsx
+++ src/component/Information.jsx
@@ -4,7 +4,7 @@ const Information = () => {
     return (
         <div>
             <h1 className='text-2xl font-merienda'>Information</h1>
-            <div className='container'>
+            <div className='container grid grid-cols-2'>
 
             </div>
         </div>
