198e857b6ea88bb1b797bd8fd893b2b5e149a406
salman-ctrl <zirmanvictory@gmail.com>
2025-03-06 23:03:43 +0700

    edit information section

--- src/component/Information.jsx
+++ src/component/Information.jsx
@@ -5,7 +5,7 @@ const Information = () => {
         <div>
             <h1 className='text-2xl font-merienda'>Information</h1>
             <div className='container grid grid-cols-2'>
-                <div className='bg-primary text-lg text-white'>
+                <div className='bg-primary text-lg text-white w-full h-52'>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas laboriosam autem ea dolor consequuntur amet nulla, suscipit consectetur harum eum magnam culpa officiis porro architecto facilis, laudantium possimus doloremque maxime!
                 </div>
             </div>
