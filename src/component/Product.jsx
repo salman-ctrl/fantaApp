32b520ea364d4c006d4bde300c5ac11d5ef205ce
salman-ctrl <zirmanvictory@gmail.com>
2025-03-08 23:59:11 +0700

    add testimoni

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -91,7 +91,17 @@ const Product = () => {
             <h2 className='text-4xl font-merienda text-center pt-30'>Produk Kami</h2>
             <div className='place-items-center mt-12 grid grid-cols-3'>
                 {dataProduct.map((item) => (
-                  
+                    <div
+                        key={index}
+                        data-aos="fade-up" // Animasi naik ke atas saat muncul
+                        data-aos-delay={index * 100} // Tunda animasi sesuai dengan index
+                        className='bg-white shadow-lg pb-7 mt-10 text-center rounded-xl w-72 hover:scale-105 transition-all duration-700 '>
+                        <img src={item.img} alt="" className=' pt-7 hover:scale-110 transition-all duration-500 hover:rotate-5 mx-auto' />
+                        <h1 className='text-xl font-merienda'>{item.product}</h1>
+                        <p className='text-sm text-gray-600  pt-3'>{item.deskripsi}</p>
+                        <button className='border-amber-500 border-2  text-amber-500  px-12 py-2 mt-5 rounded-sm transition-all duration-400 cursor-pointer hover:scale-110 hover:text-white hover:bg-amber-500 '>Beli</button>
+                    </div>
+
                 ))}
             </div>
 
